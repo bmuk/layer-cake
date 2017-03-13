129cddab994df571f6f4b5cd649f8a7cc2e50446
Britt Mathis <britt.mathis@gmail.com>
2017-03-13 08:24:09 -0400

    fixed routes test

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import LayerCakeRoutes from './App'
+import LayerCakeRoutes from './Routes'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
