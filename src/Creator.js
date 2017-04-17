3e6c8c750ba4e09441abace5c9ae8f6181be4c9f
Britt Mathis <britt.mathis@gmail.com>
2017-04-16 23:58:44 -0400

    app works, it's just ugly

--- src/Creator.js
+++ src/Creator.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { Container, Search } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import _ from 'lodash'
 import { find } from './youtube'
 
-const itemToResult = (item) => ({
-  title: item.snippet.title,
-  description: item.snippet.title
+const itemToResult = item => ({
+    title: item.snippet.title,
+    description: item.snippet.description,
+    videoId: item.id.videoId
 })
+
+const justVideos = item => item.id.kind === "youtube#video"
 
 class Creator extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      results: []
+        results: [],
+        visual: "",
+        audio: ""
     }
     this.clearResults = this.clearResults.bind(this)
     this.onPageLoad = this.onPageLoad.bind(this)
@@ -22,16 +29,43 @@ class Creator extends Component {
   }
 
   onPageLoad(items) {
-    this.setState({ results: this.state.results.concat(items.map(itemToResult)) })
+      const mergedItems = _.uniqBy(this.state.results.concat(items.filter(justVideos).map(itemToResult)), item => item.title)
+      this.setState({ results: mergedItems })
   }
 
   render() {
     return (
       <Container text>
-        <Search results={this.state.results} onSearchChange={(event, value) => {
-            this.clearResults()
-            find(value, 10, this.onPageLoad)
-          }}/>
+            {this.state.visual !== ""
+             ? <strong>{this.state.visual}</strong>
+             : <Search
+             results={this.state.results}
+             onResultSelect={(_, data) => {
+                 this.clearResults()
+                 this.setState({ visual: data.videoId })
+             }}
+             onSearchChange={(event, value) => {
+                 this.clearResults()
+                 find(value, 10, this.onPageLoad)
+             }}
+             />
+            }
+        {this.state.audio !== ""
+         ? <strong>{this.state.audio}</strong>
+         : (this.state.visual !== "" ? <Search
+         results={this.state.results}
+         onResultSelect={(_, data) => {
+             this.clearResults()
+             this.setState({ audio: data.videoId })
+         }}
+         onSearchChange={(event, value) => {
+             this.clearResults()
+             find(value, 10, this.onPageLoad)
+         }}
+            /> : null)
+        }
+
+        <Link to={"/view/" + this.state.visual + "/" + this.state.audio}>Bake Cake!</Link>
       </Container>
     )
   }
